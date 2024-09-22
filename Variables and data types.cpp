fc15eed4dbedc4020f61e47b4d4aae536b344be9
DeepCool <DeepCool@DESKTOP-O8K62PQ>
2024-09-22 22:17:21 +0300

    Add project files.

---
+++ Variables and data types.cpp
@@ -0,0 +1,41 @@
+using namespace std;
+#include <iostream>
+
+
+int main() {
+    //int secondsPassed;
+    //cout << "Enter the number of seconds since the start of the day: ";
+    //cin >> secondsPassed;
+
+    //int hours = secondsPassed / 3600;
+    //int minutes = (secondsPassed % 3600) / 60;
+    //int seconds = secondsPassed % 60;
+
+    //cout << "Current time: " << hours << " hours " << minutes << " minutes " << seconds << " seconds\n";
+
+    //int secondsInADay = 24 * 3600;
+    //int secondsRemaining = secondsInADay - secondsPassed;
+
+    //int hoursLeft = secondsRemaining / 3600;
+    //int minutesLeft = (secondsRemaining % 3600) / 60;
+    //int secondsLeft = secondsRemaining % 60;
+
+    //cout << "Until midnight left: " << hoursLeft << " hours "
+    //    << minutesLeft << " minutes " << secondsLeft << " seconds.\n";
+
+
+  //2
+   /*     double diameter;
+        cout << "Enter the diameter of the circle: ";
+        cin >> diameter;
+
+        double radius = diameter / 2.0;
+
+        double area = radius * radius;
+        double perimeter = radius * 2;
+
+        cout << "Area : " << area << endl;
+        cout << "Perimeter : " << perimeter << endl;*/
+}
+
+
